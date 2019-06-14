0afd8ff1987c614c032430903e0dda4cf3ceb33a
RAPTOR\misza <zexter2@wp.pl>
2019-06-14 15:52:58 +0200

    Adding gitignore and basic client get and post functions

---
+++ ShopApi/Controllers/ClientsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : Controller
+    {
+        private readonly ShopApiContext _context;
+
+        public ClientsController(ShopApiContext context)
+        {
+            _context = context;
+        }
+
+        // Returns an IEnumerable of all clients in the database
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        // Returns the client with given id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(long id)
+        {
+            var client = await _context.Clients.FindAsync(id);  
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Client>> AddClient(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetClient), new {id = client.Id}, client);
+        }
+    }
+}
